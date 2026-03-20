082cbde523d628ca71106740418af8b971eb9b59
BorysLafontaine <124840472+BorysLafontaine@users.noreply.github.com>
2026-03-20 15:41:12 -0400

    ss_intake problématique

--- src/main/java/frc/robot/subsystems/SS_Intake.java
+++ src/main/java/frc/robot/subsystems/SS_Intake.java
@@ -4,50 +4,66 @@
 
 package frc.robot.subsystems;
 
+import java.nio.file.Watchable;
+
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SS_Intake extends SubsystemBase {
-  private final PneumaticHub m_pneumaticHub;
-  private final DoubleSolenoid m_solenoid;
+
+  private final Solenoid m_solenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+  private final DoubleSolenoid m_doubleSolenoid =
+  new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
+  
   // État du système 
   private Value deployedState ;
   
   /** create a new ss_intake */
   public SS_Intake() {
-    m_solenoid = new DoubleSolenoid(1, PneumaticsModuleType.REVPH, 1, 0);
-    deployedState = m_solenoid.get();
-    m_pneumaticHub = new PneumaticHub(1);
-    m_pneumaticHub.enableCompressorDigital();
+     // Publish elements to shuffleboard.
+    ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
+    tab.add("Single Solenoid", m_solenoid);
+    tab.add("Double Solenoid", m_doubleSolenoid);
+    tab.add("Compressor", m_compressor);
+
+    // Also publish some raw data
+    // Get the pressure (in PSI) from the analog sensor connected to the PH.
+    // This function is supported only on the PH!
+    // On a PCM, this function will return 0.
+    tab.addDouble("PH Pressure [PSI]", m_compressor::getPressure);
+    // Get compressor current draw.
+    tab.addDouble("Compressor Current", m_compressor::getCurrent);
+    // Get whether the compressor is active.
+    tab.addBoolean("Compressor Active", m_compressor::isEnabled);
+    // Get the digital pressure switch connected to the PCM/PH.
+    // The switch is open when the pressure is over ~120 PSI.
+    tab.addBoolean("Pressure Switch", m_compressor::getPressureSwitchValue);
   }
 
   public void deploy_retract() {
-        if (deployedState == Value.kForward) {
-            retract();
-        } else if(deployedState == Value.kReverse){
-            deploy();
-        }
+       
     }
 
   // ─── Déploiement ─────────────────────────────────────────────────────────
   private void deploy() {
 
-      m_solenoid.set(DoubleSolenoid.Value.kForward);
-      System.out.println(" intake DEPLOYED");
+   
   }
 
   // ─── Rétractation ────────────────────────────────────────────────────────
   private void retract() {
-      m_solenoid.set(DoubleSolenoid.Value.kReverse);
-      System.out.println(" intake RETRACTED");
+   
   }
   public void update(){
-    deployedState = m_solenoid.get();
-    SmartDashboard.putString("solenoid state" , deployedState.toString());
+
   }
 
 
@@ -56,3 +72,82 @@ public class SS_Intake extends SubsystemBase {
     update();
   }
 }
+
+
+
+  // Solenoid corresponds to a single solenoid.
+  // In this case, it's connected to channel 0 of a PH with the default CAN ID.
+  
+
+  // DoubleSolenoid corresponds to a double solenoid.
+  // In this case, it's connected to channels 1 and 2 of a PH with the default CAN ID.
+
+
+  // Compressor connected to a PH with a default CAN ID (1)
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+
+  static final int kSolenoidButton = 1;
+  static final int kDoubleSolenoidForwardButton = 2;
+  static final int kDoubleSolenoidReverseButton = 3;
+  static final int kCompressorButton = 4;
+
+  /** Called once at the beginning of the robot program. */
+  public Robot() {
+    // Publish elements to shuffleboard.
+    ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
+    tab.add("Single Solenoid", m_solenoid);
+    tab.add("Double Solenoid", m_doubleSolenoid);
+    tab.add("Compressor", m_compressor);
+
+    // Also publish some raw data
+    // Get the pressure (in PSI) from the analog sensor connected to the PH.
+    // This function is supported only on the PH!
+    // On a PCM, this function will return 0.
+    tab.addDouble("PH Pressure [PSI]", m_compressor::getPressure);
+    // Get compressor current draw.
+    tab.addDouble("Compressor Current", m_compressor::getCurrent);
+    // Get whether the compressor is active.
+    tab.addBoolean("Compressor Active", m_compressor::isEnabled);
+    // Get the digital pressure switch connected to the PCM/PH.
+    // The switch is open when the pressure is over ~120 PSI.
+    tab.addBoolean("Pressure Switch", m_compressor::getPressureSwitchValue);
+  }
+
+  @SuppressWarnings("PMD.UnconditionalIfStatement")
+  @Override
+  public void teleopPeriodic() {
+    /*
+     * The output of GetRawButton is true/false depending on whether
+     * the button is pressed; Set takes a boolean for whether
+     * to retract the solenoid (false) or extend it (true).
+     */
+    m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
+
+    /*
+     * GetRawButtonPressed will only return true once per press.
+     * If a button is pressed, set the solenoid to the respective channel.
+     */
+    if (m_stick.getRawButtonPressed(kDoubleSolenoidForwardButton)) {
+      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+    } else if (m_stick.getRawButtonPressed(kDoubleSolenoidReverseButton)) {
+      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    // On button press, toggle the compressor.
+    if (m_stick.getRawButtonPressed(kCompressorButton)) {
+      boolean isCompressorEnabled = m_compressor.isEnabled();
+      if (isCompressorEnabled) {
+        m_compressor.disable();
+      } else {
+        if (false) {
+          m_compressor.enableDigital();
+        }
+        if (true) {
+          m_compressor.enableAnalog(70, 120);
+        }
+        if (false) {
+          m_compressor.enableHybrid(70, 120);
+        }
+      }
+    }
+  }
