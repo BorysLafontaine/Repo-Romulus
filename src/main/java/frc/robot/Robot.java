3f611bc5a454edea5390f3042d1ab681c785e853
mathieu <mathieu.lachapelle.personnel@gmail.com>
2026-03-20 17:33:17 -0400

    put in robot.java SS_Intake

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,23 +5,32 @@
 package frc.robot;
 
 import com.ctre.phoenix6.HootAutoReplay;
-
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import frc.robot.subsystems.SS_Intake;
+
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
-
     private final RobotContainer m_robotContainer;
 
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
         .withTimestampReplay()
         .withJoystickReplay();
+    
+    private final SS_Intake m_intake = new SS_Intake();
+    // ⚠️ port 1 pour ne pas conflit avec le xbox sur port 0
+    private final Joystick m_stick = new Joystick(1);
+    private static final int kDeployButton     = 1;
+    private static final int kRetractButton    = 2;
+    private static final int kCompressorButton = 3;
+    private static final int kSolenoidButton   = 4;
 
     public Robot() {
-        m_robotContainer = new RobotContainer();
+        m_robotContainer = new RobotContainer();   
     }
 
     @Override
@@ -32,17 +41,14 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledInit() {}
-
     @Override
     public void disabledPeriodic() {}
-
     @Override
     public void disabledExit() {}
 
     @Override
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(m_autonomousCommand);
         }
@@ -50,7 +56,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousPeriodic() {}
-
     @Override
     public void autonomousExit() {}
 
@@ -63,8 +68,21 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void teleopPeriodic() {}
-
+    public void teleopPeriodic() {  
+        // Single solenoid — maintenu tant que bouton enfoncé
+        m_intake.setSolenoid(m_stick.getRawButton(kSolenoidButton));
+        // Double solenoid — deploy ou retract
+        if (m_stick.getRawButtonPressed(kDeployButton)) {
+            m_intake.deploy();
+        } else if (m_stick.getRawButtonPressed(kRetractButton)) {
+            m_intake.retract();
+        }
+        // Toggle compressor
+        if (m_stick.getRawButtonPressed(kCompressorButton)) {
+            m_intake.toggleCompressor();
+        }
+    }
+    
     @Override
     public void teleopExit() {}
 
@@ -75,10 +93,8 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testPeriodic() {}
-
     @Override
     public void testExit() {}
-
     @Override
     public void simulationPeriodic() {}
 }
--- src/main/java/frc/robot/subsystems/SS_Intake.java
+++ src/main/java/frc/robot/subsystems/SS_Intake.java
@@ -1,152 +1,61 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
-import java.nio.file.Watchable;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SS_Intake extends SubsystemBase {
 
-  private final Solenoid m_solenoid =             new Solenoid(PneumaticsModuleType.REVPH, 0);
-  private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
-  private final Compressor m_compressor =         new Compressor(PneumaticsModuleType.REVPH);
-  // État du système 
-  private Value deployedState;
+    private final Solenoid m_solenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+    private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
+    private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
 
-  static final int kSolenoidButton = 1;
-  static final int kDoubleSolenoidForwardButton = 2;
-  static final int kDoubleSolenoidReverseButton = 3;
-  static final int kCompressorButton = 4;
-  
-  /** create a new ss_intake */
-  public SS_Intake() {
-     // Publish elements to shuffleboard.
-    ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
-    tab.add("Single Solenoid", m_solenoid);
-    tab.add("Double Solenoid", m_doubleSolenoid);
-    tab.add("Compressor", m_compressor);
-    // Also publish some raw data
-    // Get the pressure (in PSI) from the analog sensor connected to the PH.
-    // This function is supported only on the PH!
-    // On a PCM, this function will return 0.
-    tab.addDouble("PH Pressure [PSI]", m_compressor::getPressure);
-    // Get compressor current draw.
-    tab.addDouble("Compressor Current", m_compressor::getCurrent);
-    // Get whether the compressor is active.
-    tab.addBoolean("Compressor Active", m_compressor::isEnabled);
-    // Get the digital pressure switch connected to the PCM/PH.
-    // The switch is open when the pressure is over ~120 PSI.
-    tab.addBoolean("Pressure Switch", m_compressor::getPressureSwitchValue);
-  }
+    public SS_Intake() {
+        ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
+        tab.add("Single Solenoid", m_solenoid);
+        tab.add("Double Solenoid", m_doubleSolenoid);
+        tab.add("Compressor", m_compressor);
+        tab.addDouble("PH Pressure [PSI]", m_compressor::getPressure);
+        tab.addDouble("Compressor Current", m_compressor::getCurrent);
+        tab.addBoolean("Compressor Active", m_compressor::isEnabled);
+        tab.addBoolean("Pressure Switch", m_compressor::getPressureSwitchValue);
 
-  public void deploy_retract() {
-       
+        m_compressor.enableAnalog(70, 120);
     }
 
-  // ─── Déploiement ─────────────────────────────────────────────────────────
-  private void deploy() {
+    public void setSolenoid(boolean extended) {
+        m_solenoid.set(extended);
+    }
 
-   
-  }
+    public void deploy() {
+        m_doubleSolenoid.set(Value.kForward);
+    }
 
-  // ─── Rétractation ────────────────────────────────────────────────────────
-  private void retract() {
-   
-  }
-  public void update(){
+    public void retract() {
+        m_doubleSolenoid.set(Value.kReverse);
+    }
 
-  }
+    public void toggleDeploy() {
+        if (m_doubleSolenoid.get() == Value.kForward) {
+            retract();
+        } else {
+            deploy();
+        }
+    }
 
+    public void toggleCompressor() {
+        if (m_compressor.isEnabled()) {
+            m_compressor.disable();
+        } else {
+            m_compressor.enableAnalog(70, 120);
+        }
+    }
 
-  @Override
-  public void periodic() {
-    update();
-  }
+    @Override
+    public void periodic() {}
 }
-
-
-
-  // Solenoid corresponds to a single solenoid.
-  // In this case, it's connected to channel 0 of a PH with the default CAN ID.
-  
-
-  // DoubleSolenoid corresponds to a double solenoid.
-  // In this case, it's connected to channels 1 and 2 of a PH with the default CAN ID.
-
-
-  // Compressor connected to a PH with a default CAN ID (1)
-  
-
-  
-
-  /** Called once at the beginning of the robot program. */
-  public Robot() {
-    // Publish elements to shuffleboard.
-    ShuffleboardTab tab = Shuffleboard.getTab("Pneumatics");
-    tab.add("Single Solenoid", m_solenoid);
-    tab.add("Double Solenoid", m_doubleSolenoid);
-    tab.add("Compressor", m_compressor);
-
-    // Also publish some raw data
-    // Get the pressure (in PSI) from the analog sensor connected to the PH.
-    // This function is supported only on the PH!
-    // On a PCM, this function will return 0.
-    tab.addDouble("PH Pressure [PSI]", m_compressor::getPressure);
-    // Get compressor current draw.
-    tab.addDouble("Compressor Current", m_compressor::getCurrent);
-    // Get whether the compressor is active.
-    tab.addBoolean("Compressor Active", m_compressor::isEnabled);
-    // Get the digital pressure switch connected to the PCM/PH.
-    // The switch is open when the pressure is over ~120 PSI.
-    tab.addBoolean("Pressure Switch", m_compressor::getPressureSwitchValue);
-  }
-
-  @SuppressWarnings("PMD.UnconditionalIfStatement")
-  @Override
-  public void teleopPeriodic() {
-    /*
-     * The output of GetRawButton is true/false depending on whether
-     * the button is pressed; Set takes a boolean for whether
-     * to retract the solenoid (false) or extend it (true).
-     */
-    m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
-
-    /*
-     * GetRawButtonPressed will only return true once per press.
-     * If a button is pressed, set the solenoid to the respective channel.
-     */
-    if (m_stick.getRawButtonPressed(kDoubleSolenoidForwardButton)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-    } else if (m_stick.getRawButtonPressed(kDoubleSolenoidReverseButton)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
-    }
-
-    // On button press, toggle the compressor.
-    if (m_stick.getRawButtonPressed(kCompressorButton)) {
-      boolean isCompressorEnabled = m_compressor.isEnabled();
-      if (isCompressorEnabled) {
-        m_compressor.disable();
-      } else {
-        if (false) {
-          m_compressor.enableDigital();
-        }
-        if (true) {
-          m_compressor.enableAnalog(70, 120);
-        }
-        if (false) {
-          m_compressor.enableHybrid(70, 120);
-        }
-      }
-    }
